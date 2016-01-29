be5eff13b40c928209712fd4ea6d5e4f0cc6498a
Michael Y. Huh <mhuh@yoonhuh.com>
2016-01-29 13:19:19 -0800

    * Restructured MainActivity methods. * Modified activity_main.xml, content_main.xml layout files. * Added CardView layout for RecyclerView. * Changed application color style.

--- app/src/main/java/com/huhx0015/gityourissues/activities/MainActivity.java
+++ app/src/main/java/com/huhx0015/gityourissues/activities/MainActivity.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.widget.ProgressBar;
 import com.huhx0015.gityourissues.R;
+import butterknife.Bind;
 
 /**
  * Created by Michael Yoon Huh on 1/29/2016.
@@ -19,21 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
+    @Bind(R.id.git_main_activity_fab_button) FloatingActionButton gitFabButton;
+    @Bind(R.id.git_main_activity_progress_indicator) ProgressBar gitProgressBar;
+    @Bind(R.id.git_main_activity_recycler_view) RecyclerView gitRecyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        initLayout();
+        initButtons();
     }
 
     @Override
@@ -56,5 +53,23 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initLayout() {
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    private void initButtons() {
+
+        // FLOATING ACTION BUTTON:
+        gitFabButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 }
